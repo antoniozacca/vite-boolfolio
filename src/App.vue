ef07a3807eb8b15650d7363282bc815e8e340d33
antoniozacca <antoniozaccaria057@gmail.com>
2024-07-30 16:45:07 +0200

    feat: axios

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@ import axios from 'axios';
 export default {
   name: 'projects',
 
-},
-methods:{
-  getProject(){
-    const result = axios.get('http://127.0.0.1:8000/api/projects')
+  methods:{
+    getProject(){
+      const result = axios.get('http://127.0.0.1:8000/api/projects')
+    }
   }
-}
+},
 </script>
 
 <template>
